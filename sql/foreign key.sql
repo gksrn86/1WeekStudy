b004c1d905b63905ef17248254ca9efc719e4457
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2019-06-10 00:13:46 +0900

    Update foreign key.sql

--- sql/foreign key.sql
+++ sql/foreign key.sql
@@ -1,3 +1,39 @@
-select avg(colume_name)
-from table_name
-where condition;
+//CREATE TABLE
+//MYSQL
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+); 
+//ORACLE
+CREATE TABLE Orders (
+    OrderID int NOT NULL PRIMARY KEY,
+    OrderNumber int NOT NULL,
+    PersonID int FOREIGN KEY REFERENCES Persons(PersonID)
+); 
+//MYSQL, ORACLE
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
+    REFERENCES Persons(PersonID)
+); 
+//ALTER TABLE
+//MYSQL, ORACLE
+ALTER TABLE Orders
+ADD FOREIGN KEY (PersonID) REFERENCES Persons(PersonID); 
+//MYSQL, ORACLE
+ALTER TABLE Orders
+ADD CONSTRAINT FK_PersonOrder
+FOREIGN KEY (PersonID) REFERENCES Persons(PersonID); 
+//DROP
+//MYSQL
+ALTER TABLE Orders
+DROP FOREIGN KEY FK_PersonOrder; 
+//ORACLE
+ALTER TABLE Orders
+DROP CONSTRAINT FK_PersonOrder; 
