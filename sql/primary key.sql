0581695a1cc942df482289fe38d6e33e8d40e5cd
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2019-06-10 00:11:14 +0900

    Update primary key.sql

--- sql/primary key.sql
+++ sql/primary key.sql
@@ -1,3 +1,39 @@
-select avg(colume_name)
-from table_name
-where condition;
+//CREATE TABLE
+
+// MYSQL
+CREATE TABLE Persons (
+  ID int NOT NULL,
+  LastName varchar(255) NOT NULL,
+  FirstName varchar(255),
+  Age int, 
+  PRIMARY KEY(ID)
+);
+// ORACLE
+CREATE TABLE Persons (
+    ID int NOT NULL PRIMARY KEY,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int
+); 
+//MYSQL, ORACLE
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    CONSTRAINT PK_Person PRIMARY KEY (ID,LastName)
+); 
+//ALTER TABLE
+// MYSQL , ORACLE
+ALTER TABLE Persons
+ADD PRIMARY KEY (ID); 
+// MYSQL , ORACLE
+ALTER TABLE Persons
+ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName); 
+//DROP
+//MYSQL
+ALTER TABLE Persons
+DROP PRIMARY KEY; 
+//ORACLE
+ALTER TABLE Persons
+DROP CONSTRAINT PK_Person; 
