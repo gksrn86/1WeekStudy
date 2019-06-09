fc2d39ac0f96c94c138c7bff4a33c805f3772f41
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2019-06-09 22:59:15 +0900

    Update and rename javascipt/index.js to javascipt/ModifyingDom/dom.js

---
+++ javascipt/ModifyingDom/dom.js
@@ -0,0 +1,6 @@
+const title = document.querySelector("#title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "I own you now";
+
+//dom에 대해서 modifying을 통해서 document의 여러가지의 함수들을 사용할수 있다는 것을 보여주는 예제 (타이틀, 바디의 글씨 색깔 등등 )
