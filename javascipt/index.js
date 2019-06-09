2e454737e140850040a6c6bd0a30e14734b82d26
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2019-06-09 21:56:51 +0900

    Add files via upload

---
+++ javascipt/index.js
@@ -0,0 +1,27 @@
+
+const nicoInfo = {
+    name: "Nico",
+    age:33,
+    gender:"Male",
+    isHandsome:true,
+    favMovies:["Along the gods", "LOTR", "Oldboy"],
+    favFood: [
+      {
+        name:"Kimchi",
+        fatty:false
+      },
+      {
+        name:"Cheese Burger",
+        fatty:true
+      }
+    ]
+  }
+console.log(nicoInfo);
+
+/*
+console.log(nicoInfo.gender);
+
+nicoInfo.gender = "Female"
+
+console.log(nicoInfo.gender);
+*/
