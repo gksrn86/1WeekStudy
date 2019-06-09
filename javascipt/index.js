2d707c53ec7d80fb3ebffdb20f8d7371a165c6e9
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2019-06-09 19:54:30 +0900

    Add files via upload

---
+++ javascipt/index.js
@@ -0,0 +1,14 @@
+/*
+const monday=  "Mon";
+const tue=  "Tue";
+const wed=  "Wed";
+const thu=  "Thu";
+const fri=  "Fri";
+
+console.log(monday,tue,wed,thu,fri);
+*/
+
+//Array
+const dayOfWeek = ["Mon","Thu","Wed","Thu","Fri","Sat","Sun", true];
+
+console.log(dayOfWeek[2]);
