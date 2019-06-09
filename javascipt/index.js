e89b3d2f2751f1f6c8c2fae3e4f9cdd45b2a711c
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2019-06-09 19:38:05 +0900

    Add files via upload

---
+++ javascipt/index.js
@@ -0,0 +1,23 @@
+// to do : finish this
+/*
+to do : finish this
+to do : finish this
+to do : finish this
+to do : finish this
+*/
+
+/* String
+const what = "Nicolas";
+console.log(what);
+*/
+/*
+Boolean
+const wat = true;
+*/
+/*
+Number
+const wat = 666;
+*/
+/*
+const wat = 55.1;
+*/
