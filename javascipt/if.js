241ccfe5002b8c59fdba226b84922a41a541bffc
gksrn86 <43736348+gksrn86@users.noreply.github.com>
2019-06-18 23:58:01 +0900

    Add files via upload

---
+++ javascipt/if.js
@@ -0,0 +1,65 @@
+/*if(10 === 5){
+  console.log("hi");
+} else{
+  console.log("ho");
+}
+
+if(true){
+  console.log("hi");
+} else{
+  console.log("ho");
+}
+
+
+if("nicolas" === "nicolas"){
+  console.log("hi");
+} else{
+  console.log("ho");
+}
+
+if("10" === 10){
+  console.log("hi");
+} else{
+  console.log("ho");
+}
+
+if("10" === 10){
+  console.log("hi");
+} else if ("10" ==="11"){
+  console.log("lalala");
+    console.log("ho");
+}
+
+&& ||
+if(20 > 5 || "nicolas" === "nicolas"){
+  console.log("yes");
+}else{
+  console.log("no");
+}
+
+true && true = true;
+false && true = false;
+true && false = false;
+false && false =false;
+
+true || true = true;
+false || true = true;
+true || false = true;
+false || false =false;
+
+ prompt("Ask Something");
+
+const age = prompt("How old are you");
+
+console.log(age);
+
+*/
+const age = prompt("How old are you");
+
+if(age >= 18 && age <= 21){
+  console.log("you can drink but you should not");
+}else if(age > 21){
+  console.log("go ahead");
+}else{
+  console.log("too young");
+}
